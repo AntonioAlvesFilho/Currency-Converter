2cfce8758de14205874cd4da97c7d3c8c4bfa933
Antonio Alves <antonioalves987654321@gmail.com>
2022-02-09 21:22:08 -0300

    final-commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Currency-Converter
+Conversor de moedas feito para fins didaticos
+
+feito por: Antonio Alves
+
+<img src="./assets/img.png" width="1000" height="600">
---
+++ scripts.js
@@ -0,0 +1,72 @@
+const button = document.getElementsByTagName("button")[0]
+const selectBotName = document.getElementById('bot-currency-select')
+
+const dolar = 5.3
+const real = 1
+const euro = 6
+const bitcoin = 230000
+
+
+const convert = () => {
+
+    const inputAmount = document.getElementById('valor').value
+    const realValue = document.getElementById('real-value')
+
+    realValue.innerHTML = new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+    }).format(inputAmount)
+
+    const dolarValue = document.getElementById('dolar-value')
+
+
+    if (selectBotName.value === 'Euro - €') {
+
+        dolarValue.innerHTML = new Intl.NumberFormat('de-DE', {
+            style: 'currency',
+            currency: 'EUR'
+        }).format(inputAmount / euro)
+    } else if (selectBotName.value === 'Real Brasileiro - R$') {
+        dolarValue.innerHTML = new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+        }).format(inputAmount / real)
+    } else if (selectBotName.value === 'BitCoin - ₿') {
+        dolarValue.innerHTML = new Intl.NumberFormat('de-DE', {
+            style: 'currency',
+            currency: 'BTC'
+        }).format(inputAmount / bitcoin)
+    } else if (selectBotName.value === 'Dólar Americano - $') {
+        dolarValue.innerHTML = new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD'
+        }).format(inputAmount / dolar)
+    }
+}
+
+const currencyChange = () => {
+    const botCurrencyName = document.getElementById('bot-currency-name')
+    const botCurrencyFlag = document.getElementById('bot-currency-flag')
+    if (selectBotName.value === 'Euro - €') {
+        botCurrencyName.innerHTML = 'Euro'
+        botCurrencyFlag.src = "./assets/eur.png"
+
+    } else if (selectBotName.value === 'Real Brasileiro - R$') {
+        botCurrencyName.innerHTML = 'Real Brasileiro'
+        botCurrencyFlag.src = "./assets/bra.png"
+
+    } else if (selectBotName.value === 'Dólar Americano - $') {
+        botCurrencyName.innerHTML = 'Dólar Americano'
+        botCurrencyFlag.src = "./assets/eua.png"
+
+    } else if (selectBotName.value === 'BitCoin - ₿') {
+        botCurrencyName.innerHTML = 'BitCoin'
+        botCurrencyFlag.src = "./assets/bit.png"
+
+    }
+    convert()
+}
+
+button.addEventListener('click', convert)
+
+selectBotName.addEventListener('change', currencyChange)
